375757612411d79af3573021286c9ff251dd2e67
Jaborov <dzhaborov.b@gmail.com>
2024-04-11 08:58:27 +0500

    24:16

---
+++ controllers/users.js
@@ -0,0 +1,13 @@
+export const login = async (req, res) => {
+    res.send('login');
+  };
+
+export const register = async (req, res) => {
+    res.send('register');
+  };
+
+export const current = async (req, res) => {
+    res.send('current');
+  };
+
+ 
